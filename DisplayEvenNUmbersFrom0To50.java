4521322b3edfdf2ab04c46118ffd5768fb8a6f3b
SaitovN <nuriksaitov03@gmailcom>
2023-06-02 15:43:53 +0600

    Create DisplayEvenNUmbersFrom0To50.java

---
+++ DisplayEvenNUmbersFrom0To50.java
@@ -0,0 +1,12 @@
+package academy.devonline.java.basic.section01_setup.section05_setup;
+
+public class DisplayEvenNUmbersFrom0To50 {
+    public static void main(String[] args) {
+        var i = 50;
+        var e = 0;
+        var r = 2;
+        for (var d = e; d <=i; d +=r){
+            System.out.print(d+" ");
+        }
+    }
+}
